e597d196b5ec4030f6e9401ca001ec68b985afbc
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-12-19 20:16:30 +0200

    main

--- src/Screens/Main/index.js
+++ src/Screens/Main/index.js
@@ -3,72 +3,45 @@ import { View } from 'react-native';
 import CircleButton from '../../Components/CircleButton';
 import TodoBlock from '../../Components/TodoBlock';
 import styles from './styles';
-
-const intialTodoData = [
-  {
-    id: '1',
-    isCompleted: false,
-    name: 'Задача 1',
-    category: 'Финансы_1',
-  },
-  {
-    id: '2',
-    isCompleted: true,
-    name: 'Задача 2',
-    category: 'Финансы_2',
-  },
-  {
-    id: '3',
-    isCompleted: false,
-    name: 'Задача 3',
-    category: 'Финансы_3',
-  },
-  {
-    id: '4',
-    isCompleted: true,
-    name: 'Задача 4',
-    category: 'Финансы_4',
-  },
-  {
-    id: '5',
-    isCompleted: false,
-    name: 'Задача 5',
-    category: 'Финансы_5',
-  },
-];
-
-const intialTask = {
-  id: null,
-  name: 'Новая задача',
-  category: null,
-  isCompleted: false,
-};
+import { todoProps } from '../../Constants/Todo';
+import { categoriesData, initialTodoData } from '../../Constants/todoStructure';
 
 const generateId = () => Math.floor(Math.random() * 1000);
 
+const initialTask = {
+  [todoProps.id]: null,
+  [todoProps.name]: 'Новая задача',
+  [todoProps.category]: null,
+  [todoProps.isCompleted]: false,
+};
+
 const Main = () => {
-  const [todoData, setTodoData] = useState(intialTodoData);
+  const [todoData, setTodoData] = useState(initialTodoData);
 
   const addNewTask = () => {
     setTodoData([
       ...todoData,
       {
-        ...intialTask,
-        id: generateId().toString(),
+        ...initialTask,
+        [todoProps.id]: generateId().toString(),
       },
     ]);
-    //  console.log('task', task);
+  };
+
+  const deleteTask = ({ id }) => {
+    const newTodoData = todoData.filter(item => item.id !== id);
+    setTodoData(newTodoData);
   };
 
   const filterTodoData = ({ data, status }) =>
-    data.filter(item => item.isCompleted === status);
+    data?.filter(item => item.isCompleted === status);
 
-  const setTaskCompleted = ({ isChacked, id }) => {
+  const onChangeTask = ({ key, value, id }) => {
     const newTodoData = todoData.map(item => {
       if (item.id === id) {
         return {
           ...item,
-          isCompleted: isChacked,
+          [key]: value,
         };
       } else {
         return item;
@@ -82,12 +55,16 @@ const Main = () => {
       <TodoBlock
         todoData={filterTodoData({ data: todoData, status: false })}
         title={'Новые!'}
-        setTaskCompleted={setTaskCompleted}
+        onChangeTodo={onChangeTask}
+        todoCategories={categoriesData}
+        onDeleteTodo={deleteTask}
       />
       <TodoBlock
         todoData={filterTodoData({ data: todoData, status: true })}
         title={'Выполненные!'}
-        setTaskCompleted={setTaskCompleted}
+        onChangeTodo={onChangeTask}
+        todoCategories={categoriesData}
+        onDeleteTodo={deleteTask}
       />
       <CircleButton add={addNewTask} />
     </View>
